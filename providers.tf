bc32b679423283eb1bbc5b652341f30a13c607d3
Cobus Bernard <cobusb@amazon.com>
2020-09-15 15:21:52 +0200

    And here we go

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  version                 = "~> 2.0"
-  region                  = var.aws_region
+  version = "~> 2.0"
+  region  = var.aws_region
 
   assume_role {
     role_arn = "arn:aws:iam::${var.aws_account_id}:role/administrator"
